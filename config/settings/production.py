b4b2c02e76e9a81642cd91fba2214851fdb519da
Jeckonia Kwasa <inctifra@gmail.com>
2024-05-14 21:20:10 +0300

    boto setup fro region with email

--- config/settings/production.py
+++ config/settings/production.py
@@ -148,7 +148,7 @@ INSTALLED_APPS += ["anymail"]
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 # https://anymail.readthedocs.io/en/stable/esps/amazon_ses/
-EMAIL_BACKEND = "anymail.backends.amazon_ses.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 ANYMAIL = {}
 
 # django-compressor
